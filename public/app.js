dd4ba2b7288b2f381fe85b1d650f3097e3cefb39
Aniela M Wolkonowski <anielamw@gmail.com>
2016-12-10 19:17:41 -0500

    initial commit

--- public/app.js
+++ public/app.js
@@ -1,27 +1,27 @@
 var errorMessage = function () {
-  alert('Heavens to Murgatroyd! You forgot to type in the field. So exit, stage left.')
+  alert('Heavens to Murgatroyd! You forgot to type in the field. So exit, stage left.');
 }
 
 var submitClick = function (event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  var newItem = $('#grocery_name').val()
+  var newItem = $('#grocery_name').val();
 
-  var listItem = function () {
-    $('ul').append('<li>' + newItem + '</li>')
-  }
+  var listItem = function () {$('ul').append('<li>' + newItem + '</li>');}
 
   var postRequest = function () {
     $.ajax({
       method: 'POST',
       url: '/groceries',
-      data: { name: newItem }
-    }).success(listItem)
+      data: {name: newItem}
+    }).success(listItem);
   }
 
-  if (newItem.trim() === '') { errorMessage() } else { postRequest() }
+  if (newItem.trim() === '') {errorMessage()}
+  else {postRequest()};
 }
 
+
 $(document).ready(function () {
-  $('form').on('submit', submitClick())
-})
+  $('form').on('submit', submitClick);
+});
