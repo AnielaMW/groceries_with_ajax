5b1e54fd6b3091cb88329ec537da8fa8edbc1be9
Aniela M Wolkonowski <anielamw@gmail.com>
2016-12-10 22:24:15 -0500

    added delete code to app.js, but it doesn't work yet

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
-var errorMessage = function () {
-  alert('Heavens to Murgatroyd! You forgot to type in the field. So exit, stage left.');
-}
-
 var submitClick = function (event) {
   event.preventDefault();
 
+  var errorMessage = function () {
+    alert('Heavens to Murgatroyd! You forgot to type in the field. So exit, stage left.');
+  }
+
   var newItem = $('#grocery_name').val();
 
-  var listItem = function () {$('ul').append('<li>' + newItem + ' <input type="button" id="remove_item" value="Delete"></input></li>');}
+  var listItem = function () {$('ul').append('<li>' + newItem + ' <input type="button" id="remove_item" value="Delete"</input></li>');}
 
   var postRequest = function () {
     $.ajax({
@@ -21,7 +21,39 @@ var submitClick = function (event) {
   else {postRequest()};
 }
 
+//------------------------------------------------
+
+var deleteClick = function (event) {
+  event.preventDefault();
+
+  var item = $(this).closest('li').val();
+  // ^^^ I know this is wrong, because it is calling the item and the delete button.
+
+  var confirmMessage = function () {
+    confirm('You are about to delete ' + item + '.');
+  } // ^^^ I'm not exctly sure how this confirm method works. I know it will ask the user to confirm their request, but does the confirm method know what to do when ok or cancle are clicked?
+
+  var deleteItem = function () {
+    $(this).closest('li').remove();
+    // ^^^ I know this is wrong, but I don't know why.
+  }
+
+  var deleteRequest = function () {
+    confirmMessage();
+    $.ajax({
+      method: 'DELETE',
+      url: '/groceries',
+      data: {name: item}
+                // ^^^ I need "item" to call the correct .val() in order for it to delete the correct record from the SQL. Is this the corrct way to create the params for my DELETE method in the app.rb?
+    }).success(deleteItem());
+  }
+
+  deleteRequest()
+}
+
+//------------------------------------------------
 
 $(document).ready(function () {
   $('form').on('submit', submitClick);
+  $('li').on('click', '#remove_item', deleteClick);
 });
